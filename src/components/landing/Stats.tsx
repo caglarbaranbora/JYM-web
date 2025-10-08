e6d021e819fc3cb185e90d3acd77cc8248047b84
caglarbaranbora <boracaglarbaran@gmail.com>
2025-10-08 17:33:23 +0300

    Add Stats component to landing page, featuring engaging statistics and visual elements to enhance user motivation and interaction. This addition improves the overall user experience and provides valuable insights into fitness goals and achievements.

---
+++ src/components/landing/Stats.tsx
@@ -0,0 +1,276 @@
+export default function Stats() {
+  return (
+    <div className="bg-black p-8 relative">
+      <div className="text-center mb-8">
+        <h2 className="text-white text-8xl font-bold mb-4">
+          Dream <span className="text-9xl text-accent">Bigger</span>, Achieve
+          Beyond
+        </h2>
+        <p className="text-gray-400 mx-auto text-3xl">
+          Push past your limits and turn ambition into reality with every rep!
+        </p>
+      </div>
+      {/* Grid container */}
+      <div className="grid grid-rows-3 gap-4 mb-8 relative">
+        {/* 1. satır */}
+        <div className="grid grid-cols-[1fr_1fr_2fr] gap-4 h-100 relative">
+          <div className="bg-[#222222] flex items-center justify-center rounded-[80px] text-6xl text-center p-16 font-semibold ">
+            Nutrition Statistics
+          </div>
+          <div className="bg-[#222222] flex items-center justify-center rounded-[80px] font-semibold text-6xl text-center p-16">
+            Fitness Goals
+          </div>
+          <div className="flex items-center justify-center rounded-[80px] font-semibold">
+            <div className="flex items-center justify-center overflow-hidden">
+              <svg
+                width="250"
+                height="250"
+                viewBox="0 0 200 200"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                {" "}
+                <g clipPath="url(#clip0_105_284)">
+                  {" "}
+                  <path
+                    fillRule="evenodd"
+                    clipRule="evenodd"
+                    d="M0 0H50V50H0V0ZM100 50H50V100H0V150H50V200H100V150H150V200H200V150H150V100H200V50H150V0H100V50ZM100 100H150V50H100V100ZM100 100V150H50V100H100Z"
+                    fill="url(#paint0_linear_105_284)"
+                  />{" "}
+                </g>{" "}
+                <defs>
+                  {" "}
+                  <linearGradient
+                    id="paint0_linear_105_284"
+                    x1="100"
+                    y1="0"
+                    x2="100"
+                    y2="200"
+                    gradientUnits="userSpaceOnUse"
+                  >
+                    {" "}
+                    <stop stopColor="#e0fe10" />{" "}
+                    <stop offset="2" stopColor="#3e25f5" />{" "}
+                  </linearGradient>{" "}
+                  <clipPath id="clip0_105_284">
+                    {" "}
+                    <rect width="200" height="200" fill="white" />{" "}
+                  </clipPath>{" "}
+                </defs>{" "}
+              </svg>
+              <svg
+                width="250"
+                height="250"
+                viewBox="0 0 200 200"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                {" "}
+                <g clipPath="url(#clip0_105_284)">
+                  {" "}
+                  <path
+                    fillRule="evenodd"
+                    clipRule="evenodd"
+                    d="M0 0H50V50H0V0ZM100 50H50V100H0V150H50V200H100V150H150V200H200V150H150V100H200V50H150V0H100V50ZM100 100H150V50H100V100ZM100 100V150H50V100H100Z"
+                    fill="url(#paint0_linear_105_284)"
+                  />{" "}
+                </g>{" "}
+                <defs>
+                  {" "}
+                  <linearGradient
+                    id="paint0_linear_105_284"
+                    x1="100"
+                    y1="0"
+                    x2="100"
+                    y2="200"
+                    gradientUnits="userSpaceOnUse"
+                  >
+                    {" "}
+                    <stop stopColor="#3e25f5" />{" "}
+                    <stop offset="2" stopColor="#e0fe10" />{" "}
+                  </linearGradient>{" "}
+                  <clipPath id="clip0_105_284">
+                    {" "}
+                    <rect width="200" height="200" fill="white" />{" "}
+                  </clipPath>{" "}
+                </defs>{" "}
+              </svg>
+              <svg
+                width="250"
+                height="250"
+                viewBox="0 0 200 200"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                {" "}
+                <g clipPath="url(#clip0_105_284)">
+                  {" "}
+                  <path
+                    fillRule="evenodd"
+                    clipRule="evenodd"
+                    d="M0 0H50V50H0V0ZM100 50H50V100H0V150H50V200H100V150H150V200H200V150H150V100H200V50H150V0H100V50ZM100 100H150V50H100V100ZM100 100V150H50V100H100Z"
+                    fill="url(#paint0_linear_105_284)"
+                  />{" "}
+                </g>{" "}
+                <defs>
+                  {" "}
+                  <linearGradient
+                    id="paint0_linear_105_284"
+                    x1="100"
+                    y1="0"
+                    x2="100"
+                    y2="200"
+                    gradientUnits="userSpaceOnUse"
+                  >
+                    {" "}
+                    <stop stopColor="#3e25f5" />{" "}
+                    <stop offset="2" stopColor="#e0fe10" />{" "}
+                  </linearGradient>{" "}
+                  <clipPath id="clip0_105_284">
+                    {" "}
+                    <rect width="200" height="200" fill="white" />{" "}
+                  </clipPath>{" "}
+                </defs>{" "}
+              </svg>
+            </div>
+          </div>
+        </div>
+
+        {/* 2. satır */}
+        <div className="grid grid-cols-[1fr_2fr_1fr] gap-4 h-100">
+          <div className=" flex items-center justify-center rounded-[80px] text-xl font-semibold">
+            <svg
+              width="300"
+              height="300"
+              viewBox="0 0 200 200"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              {" "}
+              <g clipPath="url(#clip0_238_1161)">
+                {" "}
+                <path
+                  fillRule="evenodd"
+                  clipRule="evenodd"
+                  d="M0 0C27.6142 0 50 22.3857 50 50C22.3858 50 0 27.6143 0 0ZM100 0C72.3858 0 50 22.3857 50 50C22.3858 50 0 72.3857 0 100C0 127.614 22.3858 150 50 150C22.3858 150 0 172.386 0 200C27.6142 200 50 177.614 50 150C50 177.614 72.3858 200 100 200C127.614 200 150 177.614 150 150C150 177.614 172.386 200 200 200C200 172.386 177.614 150 150 150C177.614 150 200 127.614 200 100C200 72.3857 177.614 50 150 50C177.614 50 200 27.6143 200 0C172.386 0 150 22.3857 150 50C150 22.3857 127.614 0 100 0ZM150 150C150 122.386 127.614 100 100 100C100 127.614 122.386 150 150 150ZM100 100C127.614 100 150 77.6143 150 50C150 77.6143 172.386 100 200 100C172.386 100 150 122.386 150 150C122.386 150 100 172.386 100 200C100 172.386 77.6142 150 50 150C77.6142 150 100 127.614 100 100ZM100 100C72.3858 100 50 122.386 50 150C50 122.386 27.6142 100 0 100C27.6142 100 50 77.6143 50 50C50 77.6143 72.3858 100 100 100ZM100 100C100 72.3857 122.386 50 150 50C122.386 50 100 27.6143 100 0C100 27.6143 77.6142 50 50 50C77.6142 50 100 72.3857 100 100Z"
+                  fill="url(#paint0_linear_238_1161)"
+                />{" "}
+              </g>{" "}
+              <defs>
+                {" "}
+                <linearGradient
+                  id="paint0_linear_238_1161"
+                  x1="100"
+                  y1="0"
+                  x2="100"
+                  y2="200"
+                  gradientUnits="userSpaceOnUse"
+                >
+                  {" "}
+                  <stop stopColor="#3e25f5" />{" "}
+                  <stop offset="1" stopColor="#e0e0e0" />{" "}
+                </linearGradient>{" "}
+                <clipPath id="clip0_238_1161">
+                  {" "}
+                  <rect width="200" height="200" fill="white" />{" "}
+                </clipPath>{" "}
+              </defs>{" "}
+            </svg>
+          </div>
+          <div className="bg-[#222222] flex items-center justify-between rounded-[80px] text-8xl font-semibold p-16">
+            <div>
+              800+ <span className="text-accent">Exercises</span>
+            </div>
+            <div>
+              <svg
+                width="200"
+                height="200"
+                viewBox="0 0 200 200"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                {" "}
+                <g clipPath="url(#clip0_231_793)">
+                  {" "}
+                  <path
+                    fillRule="evenodd"
+                    clipRule="evenodd"
+                    d="M50 0H200V50V150L150 200L150 50H0L50 0ZM0 165.067V100L65.067 100L0 165.067ZM100 200H35.7777L100 135.778L100 200Z"
+                    fill="url(#paint0_linear_231_793)"
+                  />{" "}
+                </g>{" "}
+                <defs>
+                  {" "}
+                  <linearGradient
+                    id="paint0_linear_231_793"
+                    x1="177"
+                    y1="-9.23648e-06"
+                    x2="39.5"
+                    y2="152.5"
+                    gradientUnits="userSpaceOnUse"
+                  >
+                    {" "}
+                    <stop stopColor="#B0B9FF" />{" "}
+                    <stop offset="1" stopColor="#E7E9FF" />{" "}
+                  </linearGradient>{" "}
+                  <clipPath id="clip0_231_793">
+                    {" "}
+                    <rect width="200" height="200" fill="white" />{" "}
+                  </clipPath>{" "}
+                </defs>{" "}
+              </svg>
+            </div>
+          </div>
+          <div className="bg-[#222222] flex items-center justify-center rounded-[80px] font-semibold text-5xl text-center p-16">
+            <p>
+              100+ Public <span className="text-[#e0fe10]">Workout</span> plan
+            </p>
+          </div>
+        </div>
+
+        {/* 3. satır */}
+        <div className="grid grid-cols-[2fr_1fr_1fr] gap-4 h-100">
+          <div className="bg-[#222222] flex items-center justify-center rounded-[80px] text-center text-8xl font-semibold p-16">
+            Calorie <span className="text-[#e0fe10]">AI+</span>
+          </div>
+          <div className="bg-[#222222] flex items-center justify-center rounded-[80px] font-semibold text-5xl text-center p-16">
+            Day by day Analytics
+          </div>
+          <div className=" flex items-center justify-center rounded-[80px] text-xl font-semibold">
+            <svg
+              width="300"
+              height="300"
+              viewBox="0 0 200 200"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              {" "}
+              <path
+                fillRule="evenodd"
+                clipRule="evenodd"
+                d="M99.9994 153.877C141.147 153.877 176.851 127.54 194.627 111.673C201.789 105.28 201.789 94.597 194.627 88.204C176.851 72.3371 141.147 46 99.9994 46C58.8516 46 23.1479 72.3371 5.37163 88.2041C-1.79055 94.597 -1.79054 105.28 5.37164 111.673C23.1479 127.54 58.8516 153.877 99.9994 153.877ZM99.9994 137.57C120.783 137.57 137.631 120.722 137.631 99.9383C137.631 79.1551 120.783 62.3069 99.9994 62.3069C79.2161 62.3069 62.368 79.1551 62.368 99.9383C62.368 120.722 79.2161 137.57 99.9994 137.57Z"
+                fill="url(#paint0_linear_105_535)"
+              />{" "}
+              <defs>
+                {" "}
+                <linearGradient
+                  id="paint0_linear_105_535"
+                  x1="157.499"
+                  y1="63.2603"
+                  x2="106.827"
+                  y2="158.86"
+                  gradientUnits="userSpaceOnUse"
+                >
+                  {" "}
+                  <stop offset="0.0509862" stopColor="#e0fe10" />{" "}
+                  <stop offset="1" stopColor="#e0e0e0" />{" "}
+                </linearGradient>{" "}
+              </defs>{" "}
+            </svg>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}

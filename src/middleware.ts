dff54433d6a8ee022ac199260987a9d5baeb84fc
guneyct <guneycandeniz@gmail.com>
2025-12-07 19:27:22 +0300

    refactor: update middleware matcher to exclude api/cron routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,7 @@ import { clerkMiddleware } from "@clerk/nextjs/server";
 export default clerkMiddleware();
 export const config = {
   matcher: [
-    "/((?!.+\\.[\\w]+$|_next).*)",
+    "/((?!.+\\.[\\w]+$|_next|api/cron).*)",
     "/",
     "/(api|trpc)(.*)",
   ],
